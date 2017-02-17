b15d86fcc0e2ce39e55a027b7e44c6518d70f258
akanimax <akanimax@gmail.com>
2017-02-17 12:57:27 +0530

    Changes made to the main template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,12 @@
 package controllers
 
 import play.api.mvc._
+import play.twirl.api.Html
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.main())
+    Ok(views.html.main("The best place to create applications")
+                    (Html("Animesh is the best man in the world")))
   }
-
 }
